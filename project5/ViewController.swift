eed82c89f1339f38c62dfda4f4de733302f5e012
Anthony Beckford <abeckford532@gmail.com>
2022-11-12 12:05:42 -0500

    Learning how to read from disk: contentsOf

--- project5/ViewController.swift
+++ project5/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    // creating properties
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+            
+            if allWords.isEmpty{
+                allWords = ["silkworm"]
+            }
+           
+        }
+    
+    func startGame(){
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+            
     }
 
 
-}
+
 
